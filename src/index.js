bf8abc62c93700d9897f946168a6b5830b7d5ebc
Patrick Lesage <Patrick.Lesage@mnp.ca>
2018-11-14 01:05:15 -0500

    with html and scss

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
-console.warn('this is a console.warn');
-console.log('this is a console.log');
+console.warn('Welcome to webpack with react.');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.scss';
+
+const App = () => {
+    return (
+        <div>
+            <p>
+                We are a most promising species, Mr. Spock, as predators go. Did you know that? I frequently
+                have my doubts. I dont. Not any more. And maybe in a thousand years or so, we will be able
+                to prove it.
+            </p>
+            <small>- Captain Kirk</small>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
